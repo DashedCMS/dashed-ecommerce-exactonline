8b34e8d7e6771b5c2080aff01ee2679d0b511230
Robinvm <Robinvm@users.noreply.github.com>
2024-02-02 14:31:30 +0000

    Fix styling

--- src/Classes/Exactonline.php
+++ src/Classes/Exactonline.php
@@ -14,7 +14,7 @@ class Exactonline
 {
     public static function isConnected($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -23,7 +23,7 @@ class Exactonline
 
     public static function authenticate($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -32,7 +32,7 @@ class Exactonline
 
     public static function saveAuthentication($code, $siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -88,14 +88,14 @@ class Exactonline
                 ->danger()
                 ->send();
 
-//            trigger_error(
-//                sprintf(
-//                    'Curl failed with error #%d: %s',
-//                    $e->getCode(),
-//                    $e->getMessage()
-//                ),
-//                E_USER_ERROR
-//            );
+            //            trigger_error(
+            //                sprintf(
+            //                    'Curl failed with error #%d: %s',
+            //                    $e->getCode(),
+            //                    $e->getMessage()
+            //                ),
+            //                E_USER_ERROR
+            //            );
         }
 
         return redirect()->route('filament.dashed.pages.dashboard');
@@ -103,7 +103,7 @@ class Exactonline
 
     public static function refreshToken($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -139,7 +139,7 @@ class Exactonline
         curl_close($ch);
         $response = json_decode($content, true);
 
-        if (!isset($response['access_token'])) {
+        if (! isset($response['access_token'])) {
             if (isset($response['error_description']) && $response['error_description'] == 'Rate limit exceeded: access_token not expired') {
                 dump('rate limit, do nothing');
 
@@ -148,7 +148,7 @@ class Exactonline
             Customsetting::set('exactonline_connected', false, $siteId);
             Customsetting::set('exactonline_access_token', null, $siteId);
             Customsetting::set('exactonline_refresh_token', null, $siteId);
-            if (!Customsetting::get('exactonline_notified_of_logout', $siteId, false)) {
+            if (! Customsetting::get('exactonline_notified_of_logout', $siteId, false)) {
                 Mails::sendNotificationToAdmins('Exact is uitgelogd en moet opnieuw worden gekoppeld in Dashed');
                 Customsetting::set('exactonline_notified_of_logout', true, $siteId);
             }
@@ -177,7 +177,7 @@ class Exactonline
 
     public static function getDivision($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -229,11 +229,11 @@ class Exactonline
 
     public static function pushProduct(Product $product, $siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
-        if (!self::isConnected($siteId) || ($product->exactonlineProduct && $product->exactonlineProduct->exactonline_id)) {
+        if (! self::isConnected($siteId) || ($product->exactonlineProduct && $product->exactonlineProduct->exactonline_id)) {
             return;
         }
 
@@ -278,11 +278,11 @@ class Exactonline
         $content = json_decode($content, true);
 
         $exactonlineProduct = $product->exactonlineProduct;
-        if (!$exactonlineProduct) {
+        if (! $exactonlineProduct) {
             $exactonlineProduct = $product->exactonlineProduct()->create();
         }
 
-        if (!isset($content['d'])) {
+        if (! isset($content['d'])) {
             $exactonlineProduct->error = $content['error']['message']['value'] ?? 'Er is iets fout gegaan';
             $exactonlineProduct->save();
 
@@ -308,11 +308,11 @@ class Exactonline
 
     public static function syncProduct(Product $product, $siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
-        if (!$product->exactonlineProduct || !$product->exactonlineProduct->exactonline_id) {
+        if (! $product->exactonlineProduct || ! $product->exactonlineProduct->exactonline_id) {
             return;
         }
 
@@ -373,11 +373,11 @@ class Exactonline
 
     public static function getCustomers($siteId = null, ?string $search)
     {
-        if (!$search) {
+        if (! $search) {
             return;
         }
 
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -444,7 +444,7 @@ class Exactonline
 
     public static function getGLAccounts($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -531,11 +531,11 @@ class Exactonline
 
     public static function getItems($siteId = null, ?string $search): array
     {
-        if (!$search) {
+        if (! $search) {
             return [];
         }
 
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -631,12 +631,12 @@ class Exactonline
 
     public static function pushOrder($order)
     {
-        if (!self::isConnected($order->site_short) || !$order->exactonlineOrder || $order->exactonlineOrder->pushed == 1) {
+        if (! self::isConnected($order->site_short) || ! $order->exactonlineOrder || $order->exactonlineOrder->pushed == 1) {
             return;
         }
 
         foreach ($order->orderProducts as $orderProduct) {
-            if ($orderProduct->product && !$orderProduct->product->exactonlineProduct) {
+            if ($orderProduct->product && ! $orderProduct->product->exactonlineProduct) {
                 $order->exactonlineOrder->pushed = 2;
                 $order->exactonlineOrder->error = 'Product ' . $orderProduct->product->name . ' is not pushed to Exactonline yet';
                 $order->exactonlineOrder->save();
@@ -674,7 +674,7 @@ class Exactonline
 
                 if ($orderProduct->sku == 'payment_costs') {
                     $exactonlineProductId = Customsetting::get('exactonline_payment_costs_product_id', $order->site_short);
-                    if (!$exactonlineProductId) {
+                    if (! $exactonlineProductId) {
                         $order->exactonlineOrder->pushed = 2;
                         $order->exactonlineOrder->error = 'Betaalmethode kosten zit nog niet aan een product in Exactonline gekoppeld';
                         $order->exactonlineOrder->save();
@@ -684,7 +684,7 @@ class Exactonline
                     }
                 } elseif ($orderProduct->sku == 'shipping_costs') {
                     $exactonlineProductId = Customsetting::get('exactonline_shipping_costs_product_id', $order->site_short);
-                    if (!$exactonlineProductId) {
+                    if (! $exactonlineProductId) {
                         $order->exactonlineOrder->pushed = 2;
                         $order->exactonlineOrder->error = 'Verzend kosten zit nog niet aan een product in Exactonline gekoppeld';
                         $order->exactonlineOrder->save();
@@ -694,7 +694,7 @@ class Exactonline
                     }
                 }
 
-                if (!$vatCodeId) {
+                if (! $vatCodeId) {
                     $vatCodeId = self::getVatCodeIdForVateRate($vatRate, $order->site_short);
                 }
 
@@ -818,7 +818,7 @@ class Exactonline
 
     public static function getVatCodes($siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
@@ -868,7 +868,7 @@ class Exactonline
 
     public static function getVatCodeIdForVateRate($vatRate, $siteId = null)
     {
-        if (!$siteId) {
+        if (! $siteId) {
             $siteId = Sites::getActive();
         }
 
