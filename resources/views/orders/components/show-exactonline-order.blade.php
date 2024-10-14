e8b0244354dc79637e0a5ee4affb2eff53e6e83a
Robin van Maasakker <robin@qubiqx.com>
2024-10-14 12:52:05 +0200

    Update

--- resources/views/orders/components/show-exactonline-order.blade.php
+++ resources/views/orders/components/show-exactonline-order.blade.php
@@ -15,7 +15,7 @@
 
                         style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
                         class="fi-btn w-full relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action"
-                        >
+                >
                     Opnieuw naar Exactonline pushen
                 </button>
             </form>
@@ -23,6 +23,12 @@
             <span
                 class="w-full justify-center bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling wordt naar Exactonline gepushed
+                                </span>
+        @endif
+        @if(!Customsetting::get('exactonline_connected', $order->site_id, 0) ? true : false)
+            <span
+                class="w-full justify-center bg-red-100 text-red-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
+                                Exactonline koppeling niet actief
                                 </span>
         @endif
     @else
@@ -34,7 +40,7 @@
             <button type="submit"
                     style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
                     class="fi-btn w-full relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action">
-                    Bestelling aan Exactonline koppelen
+                Bestelling aan Exactonline koppelen
             </button>
         </form>
     @endif
